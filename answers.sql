c0fe5fb2d849a9c55137a92bf14090728a4e4435
essytech254 <esthermuthoni030@gmail.com>
2025-05-03 22:57:34 +0300

    modified week 7 answers

--- answers.sql
+++ answers.sql
@@ -1,42 +1,37 @@
- --  Question 1
-CREATE TABLE ProductDetail (
+-- Quiz 1
+CREATE TABLE productDetail_1nf (
     OrderID INT,
     CustomerName VARCHAR(100),
-    Products VARCHAR(100)
+    Product VARCHAR(50)
 );
-INSERT INTO ProductDetail(OrderID, CustomerName, Products)
-VALUES
-(101, 'John Doe', 'Laptop'),
-(101, 'John Doe', 'Mouse'),
-(102, 'Jane Smith', 'Tablet'),
-(102, 'Jane Smith', 'Keyboard'),
-(102, 'Jane Smith', 'Mouse'),
-(103, 'Emily Clark', 'Phone');
--- Question 2
-CREATE TABLE Orders (
+
+INSERT INTO productDetail_1nf (OrderID, CustomerName, Product) VALUES (101, 'John Doe', 'Laptop');
+INSERT INTO productDetail_1nf (OrderID, CustomerName, Product) VALUES (101, 'John Doe', 'Mouse');
+INSERT INTO productDetail_1nf (OrderID, CustomerName, Product) VALUES (102, 'Jane Smith', 'Tablet');
+INSERT INTO productDetail_1nf (OrderID, CustomerName, Product) VALUES (102, 'Jane Smith', 'Keyboard');
+INSERT INTO productDetail_1nf (OrderID, CustomerName, Product) VALUES (102, 'Jane Smith', 'Mouse');
+INSERT INTO productDetail_1nf (OrderID, CustomerName, Product) VALUES (103, 'Emily Clark', 'Phone');
+
+-- Quiz 2
+CREATE TABLE orders (
     OrderID INT PRIMARY KEY,
     CustomerName VARCHAR(100)
 );
-INSERT INTO Orders (OrderID, CustomerName)
-VALUES
-(101, 'John Doe'),
-(102, 'Jane Smith'),
-(103, 'Emily Clark');
 
-
-CREATE TABLE Product (
+CREATE TABLE orderDetails_2nf (
     OrderID INT,
-    Product VARCHAR(100),
+    Product VARCHAR(50),
     Quantity INT,
-    PRIMARY KEY (OrderID, Product),
     FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
 );
-INSERT INTO Product (OrderID, Product, Quantity)
-VALUES
-(101, 'Laptop', 2),
-(101, 'Mouse', 1),
-(102, 'Tablet', 3),
-(102, 'Keyboard', 1),
-(102, 'Mouse', 2),
-(103, 'Phone', 1);
 
+INSERT INTO orders (OrderID, CustomerName) VALUES (101, 'John Doe');
+INSERT INTO orders (OrderID, CustomerName) VALUES (102, 'Jane Smith');
+INSERT INTO orders (OrderID, CustomerName) VALUES (103, 'Emily Clark');
+
+INSERT INTO orderDetails_2nf (OrderID, Product, Quantity) VALUES (101, 'Laptop', 2);
+INSERT INTO orderDetails_2nf (OrderID, Product, Quantity) VALUES (101, 'Mouse', 1);
+INSERT INTO orderDetails_2nf (OrderID, Product, Quantity) VALUES (102, 'Tablet', 3);
+INSERT INTO orderDetails_2nf (OrderID, Product, Quantity) VALUES (102, 'Keyboard', 1);
+INSERT INTO orderDetails_2nf (OrderID, Product, Quantity) VALUES (102, 'Mouse', 2);
+INSERT INTO orderDetails_2nf (OrderID, Product, Quantity) VALUES (103, 'Phone', 1);
